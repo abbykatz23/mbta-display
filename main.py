ffb39661bf4f74e3f6341b154de0b9d274c963c8
abbykatz23 <abbykatz23@gmail.com>
2026-02-15 17:43:36 -0500

    Flash exclamation point for currently arriving trains

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from dateutil.tz import tzoffset
 from typing import Optional
 
 from mbta_client import MBTAClient
-from enums import StationID, RouteID
+from enums import StationID, RouteID, TextColor
 from display import Display
 from settings import COMMUTE_TIMES
 
@@ -18,28 +18,28 @@ async def poll_loop(mbta_client: MBTAClient, display: Display):
             now = datetime.now(EST)
 
             (
-                gl_north_currently_arriving,
+                b_currently_arriving,
                 b_min_to_nct_1,
                 b_min_to_nct_2,
             ) = mbta_client.get_predictions_of_interest(
                 StationID.PARK_STREET_B, now, COMMUTE_TIMES[StationID.PARK_STREET_B]
             )
             (
-                gl_north_currently_arriving,
+                c_currently_arriving,
                 c_min_to_nct_1,
                 c_min_to_nct_2,
             ) = mbta_client.get_predictions_of_interest(
                 StationID.PARK_STREET_C, now, COMMUTE_TIMES[StationID.PARK_STREET_C]
             )
             (
-                gl_north_currently_arriving,
+                d_currently_arriving,
                 d_min_to_nct_1,
                 d_min_to_nct_2,
             ) = mbta_client.get_predictions_of_interest(
                 StationID.PARK_STREET_D, now, COMMUTE_TIMES[StationID.PARK_STREET_D]
             )
             (
-                gl_north_currently_arriving,
+                e_currently_arriving,
                 e_min_to_nct_1,
                 e_min_to_nct_2,
             ) = mbta_client.get_predictions_of_interest(
@@ -108,6 +108,41 @@ async def poll_loop(mbta_client: MBTAClient, display: Display):
                 StationID.DTC_OL_FOREST_HILLS, now, COMMUTE_TIMES[StationID.DTC_OL_FOREST_HILLS]
             )
 
+            alert_color = None
+            alert_location = None
+
+            if (
+                ashmont_braintree_currently_arriving
+                or ol_s_currently_arriving
+                or b_currently_arriving
+                or c_currently_arriving
+                or d_currently_arriving
+                or e_currently_arriving
+            ):
+                alert_location = (61, 39)
+                if ashmont_braintree_currently_arriving:
+                    alert_color = TextColor.RED.value
+                elif ol_s_currently_arriving:
+                    alert_color = TextColor.ORANGE.value
+                else:
+                    alert_color = TextColor.GREEN.value
+            elif (
+                alewife_currently_arriving
+                or won_currently_arriving
+                or ol_n_currently_arriving
+                or north_d_currently_arriving
+                or north_e_currently_arriving
+            ):
+                alert_location = (2, 17)
+                if alewife_currently_arriving:
+                    alert_color = TextColor.RED.value
+                elif ol_n_currently_arriving:
+                    alert_color = TextColor.ORANGE.value
+                elif won_currently_arriving:
+                    alert_color = TextColor.BLUE.value
+                else:
+                    alert_color = TextColor.GREEN.value
+
             display.display_train_statuses(
                 b_min_to_nct_1,
                 b_min_to_nct_2,
@@ -132,6 +167,10 @@ async def poll_loop(mbta_client: MBTAClient, display: Display):
                 ol_s_min_to_nct_1,
                 ol_s_min_to_nct_2
             )
+
+
+            if alert_color and alert_location:
+                display.blink_exclamation(alert_color, location=alert_location)
 
             await asyncio.sleep(INTERVAL_SECONDS)
         except asyncio.CancelledError:
